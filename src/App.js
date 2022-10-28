61011e27595cea476d6917e66320634f4151fd58
Anetweb <anancheva2014@mail.ru>
2022-10-28 14:16:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [location, setLocation] = useState(0);
+  const {id, header, image, description} = data[location];
+
+  const назадLocation = () => {
+   setLocation((location =>{
+    location --;
+    if(location < 0) {
+      return data.length-1;
+    }
+    return location;
+   }))
+  }
+
+  const вперёдLocation = () => {
+    setLocation((location =>{
+      location ++;
+      if(location>data.length -1) {
+        location = 0;
+      }
+      return location;
+   } )
+     )
+  }
+
+
+
+  return (<div>
+    <div className="container">
+      <h1>{id}-{header}</h1>
     </div>
-  );
+
+    <div className="container">
+      <img src={image} width="700px" height="500px" alt="crimea"/>
+    </div>
+
+    <div className="container">
+      <h2>{description}</h2>
+      </div>
+
+      <div className="btn container">
+        <button onClick={назадLocation}>Назад</button>
+        <button onClick={вперёдLocation}>Вперёд</button>
+      </div>
+
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,202 @@
+export const data = [
+
+{
+    id: 1,
+    header:"Пейзажи Крыма!",
+    image:"https://images.unsplash.com/photo-1591530171091-95bba84c7093?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3JpbWVhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    description:"Мыс Фиолент"
+},
+
+{
+    id: 2,
+    header:"Пейзажи Крыма!",
+    image:"https://images.unsplash.com/photo-1586006057877-16cd7c297afb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNyaW1lYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:"Гора Ай Петри"
+},
+
+{
+    id: 3,
+    header:"Пейзажи Крыма!",
+    image:"https://images.unsplash.com/photo-1601204057702-0bf266c98ca9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNyaW1lYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:"Алупка Воронцовский Дворец"
+},
+
+{
+    id: 4,
+    header:"Пейзажи Крыма!",
+    image:"https://images.unsplash.com/photo-1598707206160-0a6b20211774?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGNyaW1lYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:"Ханский Дворец, Бахчисарай"
+},
+
+{
+    id: 5,
+    header:"Пейзажи Крыма!",
+    image:"https://images.unsplash.com/photo-1653407796358-da786332f1c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjkwfHxjcmltZWF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    description:"Новый Свет Судак"
+},
+
+{
+    id: 6,
+    header:"Пейзажи Крыма!",
+    image:"https://images.unsplash.com/photo-1643800265662-da56c937ccdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzQ4fHxjcmltZWF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    description:"Инкерманский Карьер Севастополь"
+}
+]
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
